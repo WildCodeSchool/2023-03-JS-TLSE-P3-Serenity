3ce0bb141d624f1091b9860fa35b97081aaabcfc
Marston <jimidbz@hotmail.fr>
2023-06-21 10:53:52 +0200

    S01_US02_practician_list_modal unused routes deleted and npm install

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,14 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-// const itemControllers = require("./controllers/itemControllers");
-
-// router.get("/items", itemControllers.browse);
-// router.get("/items/:id", itemControllers.read);
-// router.put("/items/:id", itemControllers.edit);
-// router.post("/items", itemControllers.add);
-// router.delete("/items/:id", itemControllers.destroy);
-
 const practicianControllers = require("./controllers/practicianController");
 
 router.get("/practicians", practicianControllers.browse);
