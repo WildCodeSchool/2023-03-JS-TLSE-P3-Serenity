473c149c150245a020cf7b3ace061f24256bfde4
Antoine JULIAN <antoine.julian@hotmail.com>
2023-07-06 09:47:42 +0200

    S02_US07 Style from and navbar

--- frontend/src/components/FormListModal.jsx
+++ frontend/src/components/FormListModal.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../styles/FormListModal.scss";
+// import DeleteButtonForm from "./DeleteButtonForm";
 
 function FormListModal() {
   const [forms, setForms] = useState([]);
@@ -28,6 +29,7 @@ function FormListModal() {
             <th>Date</th>
             <th>Lu</th>
             <th>Fait</th>
+            <th> </th>
           </tr>
         </thead>
         <tbody className="form-list-table-body">
@@ -39,6 +41,15 @@ function FormListModal() {
               <td>{new Date(form.create_time).toLocaleDateString()}</td>
               <td>{form.is_read}</td>
               <td>{form.is_done}</td>
+              <td className="practician-list-table-buttons">
+                <button
+                  type="button"
+                  className="delete-button"
+                  onClick={console.info("Delete Form")}
+                >
+                  <i className="fi fi-rr-trash" />
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
