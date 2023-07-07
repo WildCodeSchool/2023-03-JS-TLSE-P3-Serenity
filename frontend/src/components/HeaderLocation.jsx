3a0bd7d8df9aa5fa8b7435b3f028b2f8689ef8e4
Antoine JULIAN <antoine.julian@hotmail.com>
2023-07-07 10:20:01 +0200

    S02_US07 Add checkbox button and REAL logo

--- frontend/src/components/HeaderLocation.jsx
+++ frontend/src/components/HeaderLocation.jsx
@@ -1,16 +1,24 @@
 import React, { useContext } from "react";
 import "../styles/HeaderLocation.scss";
 import StateContext from "../contexts/StateContext";
+import headerLogoBlack from "../assets/header_logo_black.svg";
+import headerLogoBlackMini from "../assets/header_logo_black_mini.svg";
 
 function HeaderLocation() {
   const { activeModal } = useContext(StateContext);
+  const isMobile = window.matchMedia("(max-width: 480px)").matches;
 
   return (
     <div className="header-location">
-      <div className="header-logo">
-        <i className="fi fi-rr-layers" />
-        <p>Serenity</p>
-      </div>
+      {isMobile ? (
+        <img
+          src={headerLogoBlackMini}
+          alt="header logo"
+          className="header-logo"
+        />
+      ) : (
+        <img src={headerLogoBlack} alt="header logo" className="header-logo" />
+      )}
       <h1>{activeModal}</h1>
     </div>
   );
