12db20c8eca14e65b908bda41a252b4575108251
JIMI_BES <jimi.bes@gmail.com>
2023-07-10 12:13:16 +0200

    delete practician function fixed to delete only one practician onclick

--- frontend/src/components/DeleteButton.jsx
+++ frontend/src/components/DeleteButton.jsx
@@ -3,55 +3,47 @@ import axios from "axios";
 import Swal from "sweetalert2";
 import PropTypes from "prop-types";
 import AuthFunctionContext from "../contexts/AuthFunctionContext";
+import "../styles/PracticianListModal.scss";
 
-function DeleteButton({ selectedPracticians, setPracticians, practicians }) {
+function DeleteButton({ practician, setPracticians, practicians }) {
   const { userToken, userInfo } = useContext(AuthFunctionContext);
   const { role } = userInfo;
 
   const handleDeleteButtonClick = () => {
-    selectedPracticians.forEach((practician) => {
-      axios.delete(
-        `${import.meta.env.VITE_BACKEND_URL}/admins/practicians/${
-          practician.id
-        }`,
-        {
-          headers: {
-            Authorization: `Bearer ${userToken}`,
-            Role: `${role}`,
-          },
-        }
-      );
-      if (selectedPracticians.length > 0) {
-        Swal.fire({
-          title: "Êtes-vous sûr de vouloir supprimer ce praticien ?",
-          text: "Vous ne pourrez pas annuler cette action !",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Oui, supprimer !",
-          cancelButtonText: "Non, annuler !",
-        })
-          .then((response) => {
-            if (response.isConfirmed) {
-              console.info(response);
-              const updatedPracticians = practicians.filter(
-                (existingPractician) => existingPractician.id !== practician.id
-              );
-              setPracticians(updatedPracticians);
-              Swal.fire(
-                "Supprimé !",
-                "Le praticien a été supprimé.",
-                "success"
-              );
+    Swal.fire({
+      title: "Êtes-vous sûr de vouloir supprimer ce praticien ?",
+      text: "Vous ne pourrez pas annuler cette action !",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      background: "#242731",
+      confirmButtonText: "Oui, supprimer !",
+      cancelButtonText: "Non, annuler !",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios
+          .delete(
+            `${
+              import.meta.env.VITE_BACKEND_URL
+            }/admins/practicians/${practician}`,
+            {
+              headers: {
+                Authorization: `Bearer ${userToken}`,
+                Role: `${role}`,
+              },
             }
-          })
-
-          .catch((error) => {
-            console.error(
-              `Error deleting practician with ID ${practician.id}:`,
-              error
+          )
+          .then(() => {
+            const updatedPracticians = practicians.filter(
+              (el) => el.id !== practician
             );
+            setPracticians(updatedPracticians);
+
+            Swal.fire("Supprimé !", "Le praticien a été supprimé.", "success");
+          })
+          .catch((error) => {
+            console.error("Error deleting practician:", error);
           });
       }
     });
@@ -67,17 +59,16 @@ function DeleteButton({ selectedPracticians, setPracticians, practicians }) {
     </button>
   );
 }
+
 DeleteButton.propTypes = {
-  selectedPracticians: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number,
-      firstname: PropTypes.string,
-      lastname: PropTypes.string,
-      mail: PropTypes.string,
-      adeli_number: PropTypes.string,
-      administrator_id: PropTypes.number,
-    })
-  ).isRequired,
+  practician: PropTypes.shape({
+    id: PropTypes.number,
+    firstname: PropTypes.string,
+    lastname: PropTypes.string,
+    mail: PropTypes.string,
+    adeli_number: PropTypes.string,
+    administrator_id: PropTypes.number,
+  }).isRequired,
   setPracticians: PropTypes.func.isRequired,
   practicians: PropTypes.arrayOf(
     PropTypes.shape({
