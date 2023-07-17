ba415e8a28baa828c6fac876dc49468e4a990c2b
JIMI_BES <jimi.bes@gmail.com>
2023-07-17 14:21:47 +0200

    modal prepared

---
+++ frontend/src/components/AnticipateReleaseModal.jsx
@@ -0,0 +1,26 @@
+import "../styles/AnticipateReleaseModal.scss";
+
+function AnticipateReleaseModal() {
+  return (
+    <div className="anticipate-release-modal-container">
+      <div className="anticipate-release-modal-header">
+        <button type="button" className="return-button-modal">
+          <i className="fi fi-rr-arrow-circle-left" />
+        </button>
+        <h1>Ma préparation</h1>
+      </div>
+      <h2>Anticiper ma sortie en prenant rendez-vous</h2>
+      <p>
+        Afin de sécuriser votre retour à la maison votre chirurgien vous invite
+        à prendre rendez-vous avec les professionnels de santé suivants :
+      </p>
+      <div>
+        <p>Praticien 1</p>
+        <p>Praticien 2</p>
+        <p>Praticien 3</p>
+      </div>
+    </div>
+  );
+}
+
+export default AnticipateReleaseModal;
