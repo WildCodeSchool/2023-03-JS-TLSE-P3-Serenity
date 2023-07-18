b05c30cc2f1de5f8e85cb18e152d6071a85f0246
jeannicaise <jeannicaise2468@gmail.com>
2023-07-18 16:11:49 +0200

    US43_finish front of understand modal

--- frontend/src/components/UnderstandInterventionModal.jsx
+++ frontend/src/components/UnderstandInterventionModal.jsx
@@ -1,68 +1,59 @@
-import React, { useContext } from "react";
 import "../styles/UnderstandInterventionModal.scss";
+import React, { useContext } from "react";
 import StateContext from "../contexts/StateContext";
-import img1 from "../assets/serenity_logo.svg";
+import imglarge from "../assets/photo-1.svg";
+import img2 from "../assets/photo-2.png";
+import img3 from "../assets/img-1.png";
 
 function UnderstandInterventionModal() {
   const { setActiveTheme } = useContext(StateContext);
 
-  function handleReturnButton() {
+  const handleReturnButtonClick = () => {
     setActiveTheme(null);
-  }
+  };
+
   return (
-    <div className="preparation-understand">
-      <div className="title-preparation">
+    <div className="understand-modal-container">
+      <div className="understand-modal-header">
         <button
           type="button"
-          className="return-button-preparation"
-          onClick={handleReturnButton}
+          className="return-button-modal-understand"
+          onClick={handleReturnButtonClick}
         >
           <i className="fi fi-rr-arrow-circle-left" />
         </button>
-        <p>Ma préparation</p>
+        <h1>Ma préparation</h1>
       </div>
-
-      <button type="button" className="understand-button">
-        <p>Comprendre mon opération</p>
-      </button>
-      <p>Schémas et documentations</p>
-      <div className="schema-preparation">
+      <h2 className="understand-modal-title">Comprendre mon opération</h2>
+      <div className="understand-modal-list">
+        <p>Schémas et documentations</p>
         <div className="grid-container">
-          <img className="large-image" src={img1} alt="" />
+          <img className="large-image" src={imglarge} alt="" />
           <div className="small-images">
-            <img src={img1} alt="" />
-            <img src={img1} alt="" />
+            <img src={img2} alt="" />
+            <img src={img2} alt="" />
           </div>
         </div>
-      </div>
-
-      <p>Les videos du Dr</p>
-      <div className="lunch-3">
-        <div>
-          {" "}
-          <p className="salad-with-wheat-and">
-            Mon chirurgien me parle des croisées
-          </p>
-          <p className="time-ressource">3minutes</p>
-        </div>
-
-        <div className="img">
-          {" "}
-          <img alt="Img" src={img1} />
-        </div>
-      </div>
-      <div className="lunch-3">
-        <div>
-          {" "}
-          <p className="salad-with-wheat-and">
-            Mon chirurgien me parle des croisées
-          </p>
-          <p className="time-ressource">3minutes</p>
-        </div>
-
-        <div className="img">
-          {" "}
-          <img alt="Img" src={img1} />
+        <p>Les videos du Dr</p>
+        <div className="card-container">
+          <div className="card-container-list">
+            <div>
+              <p className="card-title">Mon chirurgien me parle des croisées</p>
+              <p className="time-ressource">3minutes</p>
+            </div>
+            <div className="Img-ressource-container">
+              <img alt="" className="Img-ressource" src={img3} />
+            </div>
+          </div>
+          <div className="card-container-list">
+            <div>
+              <p className="card-title">Mon chirurgien me parle des croisées</p>
+              <p className="time-ressource">3minutes</p>
+            </div>
+            <div className="Img-ressource-container">
+              <img alt="" className="Img-ressource" src={img3} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
